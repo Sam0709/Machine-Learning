965f66ae1c3aa459c80d1aec64776bd6cffb6769
Sam0709 <shamimmahmud09@gmail.com>
2019-10-27 00:50:06 -0500

    online

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -13,7 +13,6 @@ y = dataset.MaxTemp
 print(X,y)
 
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
@@ -22,19 +21,29 @@ regressor.fit(X_train, y_train)
 coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])
 print(coeff_df)
 
+plt.subplot(2, 1, 1)
+seabornInstance.distplot(dataset['MaxTemp'])
+
 
 y_pred = regressor.predict(X_test)
 df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 df1 = df.head(25)
 print(df1)
 
+plt.subplot(2, 1, 2)
+plt.scatter(X_test, y_test,  color='gray')
+plt.plot(X_test, y_pred, color='red', linewidth=2)
+
+
 df1.plot(kind='bar',figsize=(10,8))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 
-# plt.show()
 
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+plt.show()
